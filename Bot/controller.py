8a0760c1ddcea5b90688718936e4d770d95fd949
Beatriz de Oliveira <beatrizopiedade@outlook.com>
2025-02-05 11:49:50 -0300

    atualiza controle

--- Bot/controller.py
+++ Bot/controller.py
@@ -3,19 +3,19 @@ import requests
 class Controle:
 
     @staticmethod
-    def rastrear():
+    def consultar():
         resposta = requests.get("https://8293-177-12-9-161.ngrok-free.app") #COLOCAR O LINK CERTO
 
         if (resposta.status_code != 200):
-            dados = resposta.json()
-            return dados
-            '''
+            #dados = resposta.json()
+            #return dados
+            
             return [
                 {"rota": "Rota 1", "localizacao": "Valonguinho", "previsao": "20 min", "online": 1},
                 {"rota": "Rota 2", "localizacao": "HUAP", "previsao": "25 min", "online": 1},
                 {"rota": "Rota 3", "localizacao": "Valonguinho", "previsao": "0 min", "online": 0},
                 {"rota": "Rota 4", "localizacao": "Praia Vermelha \(Bloco H\)", "previsao": "1 hora", "online": 1},
             ]
-            '''
+
         else:
             return None
